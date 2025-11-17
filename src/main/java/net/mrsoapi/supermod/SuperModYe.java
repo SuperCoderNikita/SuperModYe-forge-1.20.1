0082d5d1c67a1818c853002dc09150419aac3968
SuperCoderNikita <kisiliovnikita@gmail.com>
2025-11-16 20:28:54 -0500

    cool commit yo

--- src/main/java/net/mrsoapi/supermod/SuperModYe.java
+++ src/main/java/net/mrsoapi/supermod/SuperModYe.java
@@ -14,6 +14,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+//Cool commit yo
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(SuperModYe.MOD_ID)
 public class SuperModYe {
